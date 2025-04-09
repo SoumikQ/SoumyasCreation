f47c0a88bd166059aec016552e5c35adb66cf77b
SOUMIK HALDER <soumik712515@gmail.com>
2025-04-09 15:39:03 +0530

    Animation Add

---
+++ app.js
@@ -0,0 +1,18 @@
+function scrollToSection(pack) {
+  const target = document.getElementById('pack');
+  if (target) {
+    target.scrollIntoView({ behavior: "smooth" });
+  }
+}
+function scrollToSection1(about) {
+    const target = document.getElementById('about');
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+  function scrollToSection2(contact) {
+    const target = document.getElementById('contact');
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  }
